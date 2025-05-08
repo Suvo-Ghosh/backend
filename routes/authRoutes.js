18424082098a06eca890a69a1e62e2506559dd01
Suvo Ghosh <suvosh663@gmail.com>
2025-05-09 03:55:41 +0530

    Remove unused auth middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
-import { protect } from '../middlewares/authMiddleware.js';
 import { registerValidation, loginValidation, validateRequest } from '../middlewares/validate.js';
 
 const router = express.Router();
